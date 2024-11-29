e1fe1efd7b6cdac748a7391c2c09013d19879ea6
LiorJigalo <audiblemaple@gmail.com>
2024-11-29 13:19:46 +0200

    improved inference stability

--- Production/hailo8/compiled_testing.py
+++ Production/hailo8/compiled_testing.py
@@ -1,5 +1,4 @@
 import cv2
-import argparse
 import time
 
 import numpy as np
@@ -10,13 +9,10 @@ from processingUtil import preprocess_face_landmarks
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Facial Landmarks Detection Example")
-    parser.add_argument("-n", "--net", help="Path for the HEF model.", default="model/face-landmarks-detection.hef", required=False)
-    parser.add_argument("-a", "--arch", help="Hailo architecture, h8, h15h", required=False)  # For future
-    args = parser.parse_args()
+    network="model/face-landmarks-detection.hef"
 
     # Load the HEF model and Get input shape
-    hailo_inference = HailoInference(args.net, input_type="UINT8", output_type='FLOAT32')
+    hailo_inference = HailoInference(network, input_type="UINT8", output_type='FLOAT32')
     input_height, input_width, _ = hailo_inference.get_input_shape()
 
     cap = init_cv_cap()
@@ -26,8 +22,6 @@ def main():
     fps_start_time = 0
     fps = 0
     class_num = 136
-    padding = 10
-
 
     while True:
         ret, frame = cap.read()
@@ -40,54 +34,52 @@ def main():
         fps = 1 / time_diff
         fps_start_time = fps_end_time
 
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
         # Run face detection without scaling
-        faces = face_cascade.detectMultiScale(frame, minNeighbors=5, minSize=(30, 30))
+        faces = face_cascade.detectMultiScale(gray_frame, minNeighbors=4, minSize=(30, 30),
+                                              flags=cv2.CASCADE_SCALE_IMAGE)
+
+        all_landmarks = []
 
         for (x, y, w, h) in faces:
             # Add padding to the face region
-            x_start = max(0, x - padding)
-            y_start = max(0, y - padding)
-            x_end = min(frame.shape[1], x + w + padding)
-            y_end = min(frame.shape[0], y + h + padding)
+            x_start = max(0, x)
+            y_start = max(0, y)
+            x_end = min(frame.shape[1], x + w)
+            y_end = min(frame.shape[0], y + h)
 
+            # Crop region of interest of the face itself
             face_roi = frame[y_start:y_end, x_start:x_end]
 
             # Preprocess the face region
             preprocessed_face = preprocess_face_landmarks(face_roi, target_size=(input_height, input_width))
 
-            all_landmarks = []
-
             # Run inference
             results = hailo_inference.run(preprocessed_face)
             landmarks_batch = results['face-landmarks-detection/fc1']
 
+            # Reshape the landmarks for the current face
+            try:
+                # Reshape to (num_landmarks, 2)
+                landmarks = landmarks_batch[0].reshape(int(class_num / 2), 2)
+            except ValueError as e:
+                raise ValueError(f"Error reshaping landmarks for face: {e}")
 
-            # Process each face's landmarks
-            for i, (x, y, w, h) in enumerate(faces):
-                # Reshape the landmarks for the current face
-                try:
-                    # Reshape to (num_landmarks, 2)
-                    landmarks = landmarks_batch[0].reshape(int(class_num / 2), 2)
-                except ValueError as e:
-                    raise ValueError(f"Error reshaping landmarks for face {i}: {e}")
+            # Adjust landmarks based on face position and size
+            landmarks = (landmarks + 0.5) * np.array([[w, h]]) + np.array([[x, y]])  # Adjust based on the face ROI
 
-                # Adjust landmarks based on face position and size
-                # Adding 0.5 as in the example for potential alignment purposes
-                landmarks = (landmarks + 0.5) * np.array([[w, h]]) + np.array([[x, y]])
+            # Append the processed landmarks to the all_landmarks list
+            all_landmarks.append(landmarks)
 
-                # Append the processed landmarks to the all_landmarks list
-                all_landmarks.append(landmarks)
-
-
-            # Draw landmarks on the frame
-            for landmarks in all_landmarks:
-                for (x, y) in landmarks:
-                    cv2.circle(frame, (int(x), int(y)), 1, (0, 255, 0), -1)
-
+        # Draw landmarks on the frame
+        for landmarks in all_landmarks:
+            for (x, y) in landmarks:
+                cv2.circle(frame, (int(x), int(y)), 1, (0, 255, 0), -1)
 
 
         # Display FPS
-        cv2.putText(frame, f"FPS: {int(fps)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (150, 150, 150), 2, cv2.LINE_AA)
+        cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (150, 150, 150), 2, cv2.LINE_AA)
 
         # Display face detection status
         face_detected_text = "Face Detected" if len(faces) > 0 else "No Face Detected"
